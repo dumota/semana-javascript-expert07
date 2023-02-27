8e057f791c7e7bb68700d8876ca670a822b8f2f4
dumota <edumota02@gmail.com>
2023-02-27 13:53:02 -0300

    add camera in page with navigator.device the javscript

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -1,10 +1,33 @@
+import Camera from "../../../shared/camera.js";
+import { supportsWorkerType } from "../../../shared/util.js";
 import Controller from "./controller.js";
 import Service from "./service.js";
 import View from "./view.js";
 
+async function getWroker() {
+    if (supportsWorkerType) {
+        console.log('suporta');
+        const worker = new Worker('./src/worker.js', { type: 'module' })
+        return worker;
+    }
+
+    const workerMock = {
+        async postMessage() { },
+        onmessage(msg) { }
+    }
+    console.log('nao suportas');
+    return workerMock;
+
+}
+
+const worker = await getWroker()
+worker.postMessage('hey from factory!')
+
+const camera = await Camera.init();
+
 
 const factory = {
-    async initialize(){
+    async initialize() {
         return Controller.initialize({
             view: new View({}),
             service: new Service({})
